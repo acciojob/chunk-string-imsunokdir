73b7a9eac8eb8f33bd3951091e7087c5003a4ecc
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-07-15 17:37:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function stringChop(str, size) {
   // your code here
-	if(str.length == ""){
+	if(str.length<=0){
 		return []
 	}
 	let newArr = []
@@ -13,4 +13,4 @@ function stringChop(str, size) {
 // Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
-alert(JSON.stringify(stringChop(str, Number(size))));
+alert(stringChop(str, Number(size)));
